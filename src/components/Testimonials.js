9059b3e7976669f5e74410baf605a244cd9f3548
sterlingrobot <sterlingrobot@gmail.com>
2019-06-23 13:38:49 -0600

    testimonials component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,27 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
 
+import './Testimonials.scss'
+
 const Testimonials = ({ testimonials }) => (
-  <div>
-    {testimonials.map(testimonial => (
-      <article key={v4()} className="message">
-        <div className="message-body">
-          {testimonial.quote}
-          <br />
-          <cite> – {testimonial.author}</cite>
-        </div>
-      </article>
-    ))}
-  </div>
+	<div className="testimonials-container">
+		{testimonials.map(testimonial => (
+			<article key={v4()} className="message">
+				<blockquote className="message-body">
+					{testimonial.quote}
+					<br />
+					<cite>
+						{testimonial.author.split(/\n/)[0]}
+						{testimonial.author.split(/\n/)[1] &&
+							<span className="author-role">{testimonial.author.split(/\n/)[1]}</span>
+						}
+					</cite>
+				</blockquote>
+			</article>
+		))}
+	</div>
 )
 
 Testimonials.propTypes = {
-  testimonials: PropTypes.arrayOf(
-    PropTypes.shape({
-      quote: PropTypes.string,
-      author: PropTypes.string,
-    })
-  ),
+	testimonials: PropTypes.arrayOf(
+		PropTypes.shape({
+			quote: PropTypes.string,
+			author: PropTypes.string,
+		})
+	),
 }
 
 export default Testimonials
