50ac715e45df223cc200e9d99f3a6a2c49bf908e
sterlingrobot <sterlingrobot@gmail.com>
2019-06-26 12:56:48 -0600

    css-purge, vars, global css

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
 		{
 			resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
 			options: {
-				develop: true, // Activates purging in npm run develop
+				develop: false, // Activates purging in npm run develop
 				purgeOnly: ['/all.scss'], // applies purging only on the bulma css file
 			},
 		}, // must be after other CSS plugins
