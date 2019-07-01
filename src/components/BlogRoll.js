f3c30f42cd501ef072ab8b256f9a8713a4c58565
sterlingrobot <sterlingrobot@gmail.com>
2019-07-01 12:56:28 -0600

    blogroll customize

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,102 +3,122 @@ import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
-class BlogRoll extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+import Facebook from '../img/social/facebook.svg'
+import Twitter from '../img/social/twitter.svg'
+import NewsIcon from '../img/news.svg'
 
-    return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
-      </div>
-    )
-  }
+import './BlogRoll.scss'
+
+class BlogRoll extends React.Component {
+	render() {
+		const { data } = this.props
+		const { edges: posts } = data.allMarkdownRemark
+
+		return (
+			<div className="columns is-multiline">
+				{posts &&
+					posts.map(({ node: post }) => (
+						<div className="column is-10 is-offset-1" key={post.id}>
+							<article className="blog-list-item">
+								<div className="blog-item-image">
+								{post.frontmatter.featuredimage ?
+									<PreviewCompatibleImage
+										imageInfo={{
+											image: post.frontmatter.featuredimage,
+											alt: `Image thumbnail for post ${post.frontmatter.title}`,
+										}}
+									/>
+									: <NewsIcon />
+								}
+								</div>
+								<div className="article-body">
+									<header>
+										<p className="post-meta">
+											<span className="is-size-5">
+												{post.frontmatter.date}
+											</span>
+											{ post.frontmatter.author &&
+												<span className="is-size-5">
+													By {post.frontmatter.author}
+												</span>
+											}
+										</p>
+										<h4 className="is-size-2">
+											<Link
+												className="title"
+												to={post.fields.slug}
+											>
+												{post.frontmatter.title}
+											</Link>
+										</h4>
+									</header>
+									<p className="article-excerpt">
+										{post.excerpt}
+										<Link className="article-readmore" to={post.fields.slug}>
+											Read more
+										</Link>
+									</p>
+									<div className="article-footer">
+										<div className="social">
+											<a title="facebook" href="https://facebook.com">
+												<Facebook />
+											</a>
+											<a title="twitter" href="https://twitter.com">
+												<Twitter />
+											</a>
+										</div>
+									</div>
+								</div>
+							</article>
+						</div>
+					))}
+			</div>
+		)
+	}
 }
 
 BlogRoll.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
+	data: PropTypes.shape({
+		allMarkdownRemark: PropTypes.shape({
+			edges: PropTypes.array,
+		}),
+	}),
 }
 
 export default () => (
-  <StaticQuery
-    query={graphql`
-      query BlogRollQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 400)
-              id
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                templateKey
-                date(formatString: "MMMM DD, YYYY")
-                featuredpost
-                featuredimage {
-                  childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
-                      ...GatsbyImageSharpFluid
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
-  />
+	<StaticQuery
+		query={graphql`
+			query BlogRollQuery {
+				allMarkdownRemark(
+					sort: { order: DESC, fields: [frontmatter___date] }
+					filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+				) {
+					edges {
+						node {
+							excerpt(pruneLength: 400)
+							id
+							fields {
+								slug
+							}
+							frontmatter {
+								title
+								templateKey
+								date(formatString: "MMMM DD, YYYY")
+								author
+								featuredpost
+								featuredimage {
+									childImageSharp {
+										fluid(maxWidth: 450, quality: 100) {
+											...GatsbyImageSharpFluid
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`}
+		render={(data, count) => <BlogRoll data={data} count={count} />}
+	/>
 )
